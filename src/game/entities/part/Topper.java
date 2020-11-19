e091e7e2f9e1fa35269790905acdea25ca9ec669
ConvertToRadians <slshumaker03@gmail.com>
2020-11-19 17:04:02 -0500

    fixed jet

--- src/game/entities/part/Topper.java
+++ src/game/entities/part/Topper.java
@@ -26,4 +26,16 @@ public class Topper extends Part {
 			drag = 1;
 		}
 	}
+
+	public static class JetTopper extends Topper {
+
+		public JetTopper(Vector pos) {
+			super(pos, Models.jetTopper);
+
+			weight = 1;
+			lift = 0;
+			thrust = 0;
+			drag = 1;
+		}
+	}
 }
